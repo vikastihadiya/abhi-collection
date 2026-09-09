47762dffe377a8a78c732eff6322ddddacb82f91
vikastihadiya <vikastihadiya@gmail.com>
2026-09-09 19:51:58 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vigcngtqcdlnozavxyva.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_oPNJfLy7fr8-vE3zOZCvCw_ctjhwuz8";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
